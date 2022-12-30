9c03692dabb816e33c59174b833d241f8678baf2
Vladyslav <vlad.dalechin@gmail.com>
2022-12-30 19:37:42 +0200

    fix: share card twitter button

--- src/components/FollowNotice.tsx
+++ src/components/FollowNotice.tsx
@@ -22,7 +22,7 @@ export const FollowNotice = (props: BoxProps) => {
       </Typography>
       <Chip
         sx={{
-          mt: isMobile ? 1.25 : 2.5,
+          my: isMobile ? 1.25 : 2.5,
           width: 'max-content',
           px: 1.875,
           py: 2.5,
@@ -37,16 +37,12 @@ export const FollowNotice = (props: BoxProps) => {
         variant="outlined"
         onClick={() => {
           window.open(
-            `https://twitter.com/intent/follow?original_referer=https%3A%2F%2F${window.location.origin}%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Efollow%7Ctwgr%5Espilnotaxyz&region=follow_link&screen_name=spilnotaxyz`,
+            `https://twitter.com/intent/follow?original_referer=${window.location.origin}%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Efollow%7Ctwgr%5Espilnotaxyz&region=follow_link&screen_name=spilnotaxyz`,
             '_blank'
           )
         }}
       />
-      <Typography
-        mt={isMobile ? 3.125 : 5.5}
-        fontSize={isMobile ? 12 : 16}
-        maxWidth={500}
-      >
+      <Typography fontSize={isMobile ? 12 : 16} maxWidth={500}>
         We want to ask you some questions. Check our twitter page there are a
         few polls there on what we should do next.
       </Typography>
--- src/components/buttons/ShareCardTwitterButton.tsx
+++ src/components/buttons/ShareCardTwitterButton.tsx
@@ -1,17 +1,19 @@
+import { Link } from '@components/Link'
 import { useTwitterData, useCardData, useGenerateWizardContext } from '@hooks'
 import { Twitter } from '@mui/icons-material'
-import { Button, ButtonProps, styled } from '@mui/material'
-import { useCallback, useState } from 'react'
+import { Button, ButtonProps } from '@mui/material'
+import { useCallback, useEffect, useState } from 'react'
 import { useAccount } from 'wagmi'
 
-export const ShareCardTwitterButton = styled((props: ButtonProps) => {
+export const ShareCardTwitterButton = (props: ButtonProps) => {
   const { state, randomColorIndex } = useGenerateWizardContext()
   const { address } = useAccount()
   const { data } = useTwitterData()
   const { data: cardData, loading: cardLoading } = useCardData()
   const [loading, setLoading] = useState(false)
+  const [url, setUrl] = useState<string | undefined | null>()
 
-  const handleClick = useCallback(async () => {
+  const getRetweetLink = useCallback(async () => {
     if (!address || !data || !data.name || !data.image || !cardData) {
       console.error(
         'address is not defined or twitter session data is not defined'
@@ -43,27 +45,52 @@ export const ShareCardTwitterButton = styled((props: ButtonProps) => {
       })
 
       const result = await response.json()
-      const url = `https://twitter.com/intent/tweet?original_referer=${window.origin}%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Er&text=HNY%202023%21%20This%20year%20I%20have%20found%20my%20favorite%20community%20%40${state.favouriteCommunity}%20and%20%40spilnotaxyz%20just%20generated%20my%20NFTing%20card%0ACheck%20it%20out%3A%20&url=spilnota.xyz/card/${result.slug}`
+      const url = `https://twitter.com/intent/tweet?original_referer=${window.location.origin}%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Er&text=HNY%202023%21%20This%20year%20I%20have%20found%20my%20favorite%20community%20%40${state.favouriteCommunity}%20and%20%40spilnotaxyz%20just%20generated%20my%20NFTing%20card%0ACheck%20it%20out%3A%20&url=spilnota.xyz/card/${result.slug}`
 
-      window.open(url, '_blank')
-    } finally {
+      setLoading(false)
+      return url
+    } catch (e) {
+      console.warn(e)
       setLoading(false)
     }
   }, [address, data, state, randomColorIndex, cardData])
 
+  useEffect(() => {
+    ;(async () => {
+      setUrl(null)
+      const url = await getRetweetLink()
+      setUrl(url)
+    })()
+  }, [getRetweetLink])
+
+  if (!url)
+    return (
+      <Button
+        disabled
+        color="secondary"
+        startIcon={<Twitter htmlColor="#1d9bf0" />}
+        {...props}
+      >
+        Share the card on Twitter
+      </Button>
+    )
+
   return (
-    <Button
-      disabled={loading || cardLoading}
-      startIcon={<Twitter htmlColor="#1d9bf0" />}
-      onClick={handleClick}
-      {...props}
+    <Link
+      href={url}
+      sx={{
+        '&:hover': { textDecoration: 'none !important' }
+      }}
+      target="_blank"
     >
-      Share the card on Twitter
-    </Button>
+      <Button
+        disabled={loading || cardLoading}
+        color="secondary"
+        startIcon={<Twitter htmlColor="#1d9bf0" />}
+        {...props}
+      >
+        Share the card on Twitter
+      </Button>
+    </Link>
   )
-})({
-  backgroundColor: '#fff',
-  '&:hover': {
-    backgroundColor: '#bababa'
-  }
-})
+}
